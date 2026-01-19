1bba7de34cf56b9ab5f6a18836fa2be4362f6260
alihaltaweel <alihaltaweel89@gmail.com>
2026-01-19 10:14:38 +0200

    Add CNN model for image classification

---
+++ src/model.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+import os
+
+MODEL_FILE = "cifar10_cnn_model.keras"
+
+def train_and_save_model():
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    model = Sequential([
+        Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)),
+        MaxPooling2D(),
+        Conv2D(64, (3,3), activation='relu'),
+        MaxPooling2D(),
+        Flatten(),
+        Dense(64, activation='relu'),
+        Dense(10, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+
+    model.save(MODEL_FILE)
+    return model
+
+def load_model():
+    if os.path.exists(MODEL_FILE):
+        return tf.keras.models.load_model(MODEL_FILE)
+    else:
+        return train_and_save_model()
